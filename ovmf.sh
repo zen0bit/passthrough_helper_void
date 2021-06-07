68712574317f13e23e36162a4de49a886919368f
zenobit <b0yc8wrv7@relay.firefox.com>
2021-06-07 06:15:02 +0200

    Update ovmf.sh
    20210421

--- ovmf.sh
+++ ovmf.sh
@@ -1,2 +1,2 @@
-wget https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-20201222.1583.g8c89229885.noarch.rpm
+wget https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-20210421.18.g15ee7b7689.noarch.rpm
 sudo cp -rv usr/share /usr/
